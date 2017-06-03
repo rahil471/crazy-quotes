20a566fd9540360a459b8b93f170915b7cc85873
Rahil Shaikh <Rahil Shaikh>
2017-06-03 11:17:12 +0530

    http request

--- src/app/the-quote/the-quote.component.ts
+++ src/app/the-quote/the-quote.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+/** New imports below */
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/observable/throw';
 
 @Component({
   selector: 'app-the-quote',
@@ -11,12 +17,23 @@ export class TheQuoteComponent implements OnInit {
     quote: String,
     by: String
   }
-  constructor() {
+  constructor( private http: Http) {
   }
 
   ngOnInit() {
-    this.myQuote.quote = "Life is about making an impact, not making an income.";
-    this.myQuote.by = "Kevin Kruse";
+    this.http.get('http://quotes.rest/qod').map((res : Response)=>{
+      return res.json();
+    })
+    .catch((err: Response|any)=>{
+      return Observable.throw(err.json());
+    }).subscribe((data)=>{      
+      this.myQuote.quote = data.contents.quotes[0].quote; //set our myQuote Object
+      this.myQuote.by = data.contents.quotes[0].author; //set our myQuote Object
+    }, (err)=>{
+      console.log(err);
+      this.myQuote.quote = "Error in fetching data"; //set our myQuote Object
+      this.myQuote.by = "Error in fetching data"; //set our myQuote Object
+    });
   }
 
 }
